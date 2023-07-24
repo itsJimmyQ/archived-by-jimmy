ed75a3731e61f2c8ddb36507e8642aa58d7ed8ae
Jimmy Qian <102806545+jimmy-qian@users.noreply.github.com>
2023-07-24 23:32:18 +0200

    [Improvement] Show loading state

--- app/components/modules/gallery/GalleryView.tsx
+++ app/components/modules/gallery/GalleryView.tsx
@@ -8,17 +8,17 @@ import { useGallery } from 'hooks';
 export const GalleryView = () => {
   const { activeTopImages, activeBottomImages, onShuffleImages, progress } = useGallery();
 
-  if (progress < 100) return null;
+  const isLoading = progress < 100;
 
   return (
     <>
       <div className="w-auto max-w-full h-full grid grid-cols-4 grid-rows-2 gap-4 mx-auto aspect-[1.375]">
-        {activeTopImages.map((image) => (
-          <GalleryImage key={image.title} {...{ image }} />
-        ))}
-        {activeBottomImages.map((image) => (
-          <GalleryImage key={image.title} {...{ image }} />
-        ))}
+        {isLoading && <h1>{progress}</h1>}
+
+        {!isLoading &&
+          activeTopImages.map((image) => <GalleryImage key={image.title} {...{ image }} />)}
+        {!isLoading &&
+          activeBottomImages.map((image) => <GalleryImage key={image.title} {...{ image }} />)}
       </div>
       <button
         className="absolute bottom-4 left-[50%] -translate-x-[50%] mx-auto p-4 border-solid border-2 border-sky-500 rounded-xl"
