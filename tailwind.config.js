16d32df695daab1dcb164f3cf5f3bae1e5909138
Jimmy Qian <jimmyqian717@gmail.com>
2023-09-11 15:03:25 +0200

    [Improvement] Add new coloirs

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,15 @@ module.exports = {
     colors: {
       grass: {
         300: '#30994A',
+        200: '#D4EECA',
+        100: '#E8F2E0',
       },
       ivory: {
+        300: '#59564C',
+        200: '#EBE9DF',
         100: '#FBFAF4',
       },
+      white: '#ffffff',
     },
     extend: {},
   },
