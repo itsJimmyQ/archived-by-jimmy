f9505ed18166eac00d8d0a986c6ca8d023b3457e
Jimmy Qian <jimmyqian717@gmail.com>
2023-10-18 18:27:42 +0200

    [Improvement] Remove menu

--- app/components/common/general/Header.tsx
+++ app/components/common/general/Header.tsx
@@ -2,25 +2,14 @@
 
 import * as React from 'react';
 
-import { AboutView } from 'modules/about';
-import { AboutButton } from 'modules/about';
-
 import { Logo } from './Logo';
 
 export const Header = () => {
-  const [isMenuOpened, setIsMenuOpened] = React.useState(false);
-
   return (
     <>
       <header className="flex justify-between items-center z-10 py-4 lg:py-6 px-6 lg:px-20 xl:px-32">
         <Logo />
-        <AboutButton
-          isActive={isMenuOpened}
-          onClick={() => setIsMenuOpened((isMenuOpened) => !isMenuOpened)}
-        />
       </header>
-
-      <AboutView isOpened={isMenuOpened} />
     </>
   );
 };
