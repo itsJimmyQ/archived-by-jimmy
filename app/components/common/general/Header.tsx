983c56174edd81487723ae7764dec15fc72ff413
Jimmy Qian <jimmyqian717@gmail.com>
2023-11-01 19:59:37 +0100

    [Fix] Remove redundant fragment

--- app/components/common/general/Header.tsx
+++ app/components/common/general/Header.tsx
@@ -8,10 +8,8 @@ import { Logo } from './Logo';
 
 export const Header = () => {
   return (
-    <>
-      <header className={clsx('flex justify-between items-center z-10', 'py-4 tablet:py-6')}>
-        <Logo />
-      </header>
-    </>
+    <header className={clsx('flex justify-between items-center z-10', 'py-4 tablet:py-6')}>
+      <Logo />
+    </header>
   );
 };
