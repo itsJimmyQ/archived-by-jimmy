d42b4da0badef5c19d0a9fb86b0f1a2bc790eaf5
Jimmy Qian <jimmyqian717@gmail.com>
2023-11-10 13:29:28 +0100

    [Feature] Console link

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 'use client';
 
+import * as React from 'react';
+
 import { GalleryFooter, GalleryView } from 'modules/gallery';
 
 const HomePage = () => {
+  React.useEffect(() => {
+    console.info(
+      'Designed and built by Jimmy ✨ \n https://www.linkedin.com/in/jimmy-qian-38b381188/',
+    );
+  }, []);
+
   return (
     <>
       <GalleryView />
